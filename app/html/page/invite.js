05ea239923f2ecc6830f9f872e70d6de51de0be8
Michael Williams <michael.williams@enspiral.com>
2017-04-14 11:40:20 +1200

    more works

--- app/html/page/invite.js
+++ app/html/page/invite.js
@@ -3,8 +3,12 @@ const ref = require('ssb-ref')
 const h = require('mutant/h')
 
 exports.gives = nest('app.html.page')
-exports.needs = nest('invite.async.accept', 'first')
+exports.needs = nest({
+  'config.sync.load': 'first',
+  'invite.async.accept': 'first'
+})
 exports.create = function (api) {
+  const loadConfig = api.config.sync.load
   const acceptInvite = api.invite.async.accept
 
   return nest('app.html.page', invitePage)
@@ -17,10 +21,16 @@ exports.create = function (api) {
 
     const progress = acceptInvite(invite, err => {
       if (err) throw err
-      localStorage.remote = data.remote
+      console.log('invited', err)
       window.location.hash = ''
       window.location.reload()
     })
+
+    // change config so we can connect to sbot
+    // TODO this should only happen after invite is successful
+    var config = loadConfig()
+    localStorage.remote = inviteData.remote
+    config.remote = inviteData.remote
 
     return h('Invitee', [
       h('h1', 'Secure Scuttlebutt'),
